e38aa572daaf39ec0a06098efd3f94e7cc6c4edf
Alex Fedenczuk <alex.fedenczuk@gmail.com>
2018-03-19 15:05:26 +0000

    added functionality to the display word_count

--- app/src/main/java/com/example/alex/wordcounter/MainActivity.java
+++ app/src/main/java/com/example/alex/wordcounter/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.alex.wordcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText userInput;
+    private Button   calculate;
+    private TextView wordCountOutput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        userInput = findViewById(R.id.input_text);
+        calculate = findViewById(R.id.wordcount_button);
+        wordCountOutput = findViewById(R.id.word_count);
     }
+
+    public void onCalculateButtonClick(View clickView){
+
+        String sentence = userInput.getText().toString();
+        WordCount wordCount = new WordCount(sentence);
+        wordCountOutput.setText("word count: " + wordCount.getWordCount());
+
+
+    }
+
 }
